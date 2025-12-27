69b7d10e4e5f75b770eb1d7a82f35ea1c86b9048
Kris <krishnakumars0101@gmail.com>
2025-12-27 23:57:26 +0530

    revise:useEffect

--- super_practice/src/components/Counter.tsx
+++ super_practice/src/components/Counter.tsx
@@ -1,15 +1,20 @@
-import { useState } from "react"
-
+import { useEffect, useState } from "react";
 
 const Counter = () => {
-    const [num,setNum] =useState(0)
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${num}`;
+    console.log("Counter updated", num);
+  }, [num]);
+
   return (
     <div>
-        <p>{num}</p>
-      <button onClick={()=>setNum(num=>num+1)} >Increment</button>
-      <button onClick={()=>setNum(num=>num-1)}>Decrement</button>
+      <p>{num}</p>
+      <button onClick={() => setNum((num) => num + 1)}>Increment</button>
+      <button onClick={() => setNum((num) => num - 1)}>Decrement</button>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
