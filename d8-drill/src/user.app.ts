1d5ae79ae7e0577a384fea10a9416042f6c093a5
kris <krishnakumars0101@gmail.com>
2026-05-21 23:41:44 +0530

    Add a user.app.ts for practicing interface,service layer and a controller layer

---
+++ d8-drill/src/user.app.ts
@@ -0,0 +1,35 @@
+export interface UserPro{
+    id:string;
+    username:string;
+    email:string
+}
+
+export interface IUserRepository{
+    save(user:UserPro):void;
+    findByEmail(email:string):UserPro | undefined;
+}
+
+
+class UserRepo implements IUserRepository {
+    private users:UserPro[] = [];
+    save(user:UserPro){
+        this.users.push(user)
+    }
+    findByEmail(email: string): UserPro | undefined {
+        return this.users.find(user =>user.email === email)
+    }
+}
+
+export class UserService {
+    constructor(private userRepo:IUserRepository){}
+
+    registerUser(newUser:UserPro):{success:boolean;message:string}{
+        if(this.userRepo.findByEmail(newUser.email)==undefined){
+            this.userRepo.save(newUser)
+            return {success:true,message:"User successfully added"}
+        }
+        else{
+            return {success:false,message:"User already registered"}
+        }
+    }
+}
